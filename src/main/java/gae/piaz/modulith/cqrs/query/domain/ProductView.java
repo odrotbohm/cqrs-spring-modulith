560ecbe2c65f6bba6dfd17f4e9a2fcb50f020243
Gaetano Piazzolla <gae.piaz@gmail.com>
2025-03-16 19:00:01 +0100

    view

--- src/main/java/gae/piaz/modulith/cqrs/query/domain/ProductView.java
+++ src/main/java/gae/piaz/modulith/cqrs/query/domain/ProductView.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "product_views")
@@ -14,6 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ProductView {
 
+    // Replicates Command fields
     @Id
     private Long id;
     private String name;
@@ -22,9 +22,9 @@ public class ProductView {
     private Integer stock;
     private String category;
     
+    // Specific to Query
     @Column(name = "average_rating")
     private Double averageRating = 0.0;
-    
     @Column(name = "review_count")
     private Integer reviewCount = 0;
 } 
