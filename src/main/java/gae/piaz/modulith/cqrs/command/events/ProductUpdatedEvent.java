9726e8f2022f591c48cbbc451c2c2f1e37f1b672
Gaetano Piazzolla <gae.piaz@gmail.com>
2025-03-17 20:03:08 +0100

    done coding (?)

--- src/main/java/gae/piaz/modulith/cqrs/command/events/ProductUpdatedEvent.java
+++ src/main/java/gae/piaz/modulith/cqrs/command/events/ProductUpdatedEvent.java
@@ -2,9 +2,6 @@ package gae.piaz.modulith.cqrs.command.events;
 
 import java.math.BigDecimal;
 
-import lombok.Builder;
-
-@Builder
 public record ProductUpdatedEvent(Long id, String name, String description, BigDecimal price, Integer stock, String category) {
 
 }
